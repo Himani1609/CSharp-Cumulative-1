9b8a839a4441fee63ef302a90361a2af32d8ce48
Himani1609 <himanibansal1691998@gmail.com>
2024-11-13 23:43:04 -0500

    Initial commit

---
+++ Csharp-Cumulative-Assignment-1/Csharp-Cumulative-Assignment-1/Controllers/TeacherPageController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Csharp_Cumulative_Assignment_1.Models;
+
+namespace Csharp_Cumulative_Assignment_1.Controllers
+{
+    public class TeacherPageController : Controller
+    {
+
+        // currently relying on the API to retrieve author information
+        // this is a simplified example. In practice, both the TeacherAPI and TeacherPage controllers
+        // should rely on a unified "Service", with an explicit interface
+        private readonly TeacherAPIController _api;
+
+        public TeacherPageController(TeacherAPIController api)
+        {
+            _api = api;
+        }
+
+        //GET : TeacherPage/List
+        public IActionResult List()
+        {
+            List<Teacher> Teachers = _api.ListTeachers();
+            return View(Teachers);
+        }
+
+        //GET : TeacherPage/Show/{id}
+        public IActionResult Show(int id)
+        {
+            Teacher SelectedTeacher = _api.FindTeacher(id);
+            return View(SelectedTeacher);
+        }
+
+
+    }
+}
